dcd6e0571080edbb780d47191d3cb9bf6e5b93fc
rukawalee <rukawalee@vip.qq.com>
2025-03-09 17:33:24 +0800

    feat: init

---
+++ src/main/java/com/boot/i/iboot3/IBoot3Application.java
@@ -0,0 +1,13 @@
+package com.boot.i.iboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IBoot3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IBoot3Application.class, args);
+	}
+
+}
