8d6fd94899b98321a78ce2dcbbd865efc9ac7cfd
rukawalee <rukawalee@vip.qq.com>
2025-03-29 00:10:15 +0800

    - Update application-datasource.yml with hardcoded database credentials - Update application-dubbo.yml with Dubbo registry settings - Remove @EnableCaching from IBoot3AppApplication - Update mybatis generator.xml with hardcoded database connection details - Update application-redis.yml with Redis host address

--- i-boot3-app/src/main/java/com/boot/i/iboot3/IBoot3AppApplication.java
+++ i-boot3-app/src/main/java/com/boot/i/iboot3/IBoot3AppApplication.java
@@ -3,10 +3,8 @@ package com.boot.i.iboot3;
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @EnableDubbo
-@EnableCaching
 @SpringBootApplication
 public class IBoot3AppApplication {
 
